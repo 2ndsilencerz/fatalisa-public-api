9469220963306a9d485373c1678b6c98f492b11c
Taufik <taufikakbarwibowo@outlook.com>
2021-10-01 21:29:41 +0700

    fix

--- service/common/pray-schedule/service.go
+++ service/common/pray-schedule/service.go
@@ -40,7 +40,7 @@ func checkTime(currentTime time.Time) bool {
 func scheduleFilesExist() bool {
 	if fileList, err := ioutil.ReadDir("/schedule"); err != nil {
 		log.Error(err)
-	} else if len(fileList) > 0 && len(fileList) == totalSchedules {
+	} else if len(fileList) > 0 && len(fileList) >= totalSchedules {
 		return true
 	}
 	log.Info("Not all schedule exist")
