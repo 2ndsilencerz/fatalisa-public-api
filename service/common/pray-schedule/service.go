b85c58671443bc91dce26a405b62f3382e3ad9b9
Taufik <taufikakbarwibowo@outlook.com>
2021-12-20 11:27:33 +0700

    update xml for pray schedule

--- service/common/pray-schedule/service.go
+++ service/common/pray-schedule/service.go
@@ -194,7 +194,7 @@ type PrayScheduleData struct {
 	Imsyak  string `xml:"imsyak" json:"imsyak" binding:"required"`
 	Fajr    string `xml:"fajr" json:"fajr" binding:"required"`
 	Syuruq  string `xml:"syuruq" json:"syuruq" binding:"required"`
-	Dzuhur  string `xml:"dzuhur" json:"dzuhur" binding:"required"`
+	Dzuhur  string `xml:"dzuhr" json:"dzuhur" binding:"required"`
 	Ashr    string `xml:"ashr" json:"ashr" binding:"required"`
 	Maghrib string `xml:"maghrib" json:"maghrib" binding:"required"`
 	Isha    string `xml:"isha" json:"isha" binding:"required"`
